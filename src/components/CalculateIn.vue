9d21d9da010d1cf87b0432d4a1f0a35173f89f4f
Kirill_13 <k1rillrodionov@yandex.ru>
2022-04-16 20:02:20 +0300

    DZ-2

--- src/components/CalculateIn.vue
+++ src/components/CalculateIn.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="calculate">
-    <h1>Калькулятор</h1>
+    <h1 class="hed">Калькулятор</h1>
     <div class="calculate_contener">
       <div class="calculate_top">
-        <input v-model ="operand1" class="bottom" type="text"> Введите 1 число
-        <input v-model ="operand2" type="text"> Введите 2 число
+        <input v-model="operand1" v-bind:disabled="display2" placeholder="Введите 1 число" class="bottom" type="text"> 
+        <input v-model ="operand2" v-bind:disabled="display" placeholder="Введите 2 число" type="text"> 
         <span> {{ result }} </span>
-        <span v-show="show"> {{ error }} </span>
+        <strong v-show="show"> {{ error }} </strong>
       </div>
       <div class="calculate_bottom">
         <button @click="step"  class="calculate_btn">Возвести в степень</button>
@@ -14,40 +14,75 @@
         <button @click="minus" class="calculate_btn">-</button>
         <button @click="um" class="calculate_btn">*</button>
         <button @click="del" v-bind:disabled="isButtonDisabled" class="calculate_btn">/</button>
+        <button @click="clear" class="calculate_btn">Очистить</button>
+      </div>
+    </div>
+    <div class="calculate_inter">
+      <input class="inter_inp" v-model="show2" type="checkbox">
+      <label>Отабразить экранную клавивтуру</label>
+      <div class="inter_form" v-if="show2">
+        <div>
+         <button class="btnn"
+            @click="get"
+            v-for="mas in masts" 
+            :key = "mas"
+            :value="mas"> 
+            {{ mas }}
+          </button>
+          <button class="remove" @click="remove">Remove</button>
+        </div>
+        <div>
+          <input type="checkbox" v-model="display">
+          <label>Операнд 1</label>
+          <input type="checkbox" v-model="display2">
+          <label>Операнд 2</label>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
+
+
 <script>
-   export default {
+  export default {
     name: "CalculateIn",
     data () {
      return {
        isButtonDisabled: false,
-       error: 'Делить на 0 нельзя!',
+       error: '',
+       masts: [1,2,3,4,5,6,7,8,9,0,'.'],
+       value: [],
+       value2: [],
        show: false,
+       show2: '',
        result: '',
        operand1: '',
-       operand2: ''
+       operand2: '',
+       display: true,
+       display2: true
       }
     },
     methods: {
       sum (){
         this.result = Number(this.operand1) + Number(this.operand2);
-        this.show = false
+        this.show = false;
+        this.isButtonDisabled = false;
       },
       minus (){
         this.result = Number(this.operand1) - Number(this.operand2);
-        this.show = false
+        this.show = false;
+        this.isButtonDisabled = false;
       },
       um (){
         this.result = Number(this.operand1) * Number(this.operand2);
-        this.show = false
+        this.show = false;
+        this.isButtonDisabled = false;
       },
       del (){
           if (Number(this.operand2) === 0) {
           this.show = true;
+          this.error = 'Делить на 0 нельзя!'
           this.isButtonDisabled = true;
           this.result = 0;
         } else if (Number(this.operand2) !== 0){
@@ -57,10 +92,42 @@
       },
       step (){
           this.result = Math.pow(Number(this.operand1), Number(this.operand2));
-          this.show = false
+          this.show = false;
+          this.isButtonDisabled = false;
+        },
+        get (e){
+          if (this.display === true && this.display2 === false){
+              this.show = false;
+              this.value.push(e.target.value);
+              this.operand1 = this.value.join('')
+          } else if (this.display2 === true && this.display === false) {
+               this.show = false;
+              this.value2.push(e.target.value);
+              this.operand2 = this.value2.join('')
+          } 
+          else if(this.display === true && this.display2 === true) {
+            this.error = "Выберете операнд";
+            this.show = true;
+          } else if (this.display === false && this.display2 === false) {
+            this.error = "Выберете операнд";
+            this.show = true;
+          }
+        },
+        remove (){
+          if (this.display === true){
+            this.value.splice(-1, 1);
+            this.operand1 = this.value.join('')
+          } else if (this.display2 === true && this.display === false) {
+            this.value2.splice(-1, 1);
+            this.operand2 = this.value2.join('')
+          }
+        },
+        clear (){
+          this.result = "";
         }
-    }
+     }
    }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -68,31 +135,69 @@
 .calculate {
       min-width: 300px;
       min-height: 300px;
-      background-color: beige;
+      background-color: rgb(217, 217, 213);
       box-shadow: 1px 1px 1px black;
 }
 .calculate_contener {
       margin:  0 auto;
-      width: 300px;
-      height: 300px;
+      width: 400px;
+      height: 250px;
       padding: 20px;
       display: flex;
       flex-direction: column;
-      justify-content: space-evenly;
+      justify-content: space-around;
       align-items: center;
 }
 .calculate_btn:not(:last-child) {
   margin-right: 10px;
 }
-input {
-  margin-button: 5px;
+.calculate_btn {
+  border: 1px solid;
+  padding: 10px;
+  color: rgb(47, 48, 49);
+}
+.calculate_top {
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  justify-content: center;
 }
 span {
-    margin-bottom: 20px;
+    width: 50px;
+    display: block;
     font-size: 20px;
     font-weight: bold;
+    margin-top: 20px;
+    align-self: center;
 }
-h1 {
-  padding-top: 10px
+.hed {
+  font-family: 'Courier New', Courier, monospace;
+  font-style: italic;
+  color: black;
+  padding-top: 20px; 
+  text-align: center;   
+}
+input {
+  padding: 10px;
+  margin-top: 5px;
+  border-radius: 5px;
+  border: none;
+}
+.inter_form {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-around;
+}
+.inter_inp {
+  margin-bottom: 10px;
+  margin-left: 20px;
+}
+.calculate_inter {
+  max-width: 400px;
+  margin: 0 auto;
+}
+.btnn:not(:last-child) {
+  margin-right: 5px;
 }
 </style>
